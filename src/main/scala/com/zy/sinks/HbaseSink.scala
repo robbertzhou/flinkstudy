fa2831ae902972160acf827458d077847ac76127
robbertzhou <1243774355@qq.com>
2020-01-17 15:16:13 +0800

    flink sink练习。

---
+++ src/main/scala/com/zy/sinks/HbaseSink.scala
@@ -0,0 +1,12 @@
+package com.zy.sinks
+
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction
+
+/**
+  * @create 2020-01-17
+  * @author zhouyu
+  * @desc hbase的sink器
+  */
+class HbaseSink extends RichSinkFunction[Object]{
+
+}
---
+++ src/main/scala/com/zy/sinks/TestSink.scala
@@ -0,0 +1,19 @@
+package com.zy.sinks
+
+import com.zy.commonapi.NoParallelSource
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.api.scala._
+
+/**
+  * @create 2020-01-17
+  * @author zhouyu
+  * @desc: 测试自定义的sink
+  */
+object TestSink {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val source = env.addSource(new NoParallelSource)
+    source.addSink(new MySink())
+    env.execute("")
+  }
+}
